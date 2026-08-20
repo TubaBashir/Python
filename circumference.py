5c09bbe6646aaf323ad3104841f3b0563292f89c
TubaBashir <tubabashir@gmail.com>
2026-08-20 09:26:36 +0530

    circum

---
+++ circumference.py
@@ -0,0 +1,8 @@
+# Define the radius of the circle
+radius = 5
+
+# Calculate the circumference using 3.14 for pi
+circumference = 2 * 3.14 * radius
+
+# Display the result
+print("The circumference is:", circumference)
