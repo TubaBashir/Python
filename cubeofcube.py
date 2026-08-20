dfd04327c79696d555baae88219496b2bdba8bf5
TubaBashir <tubabashir@gmail.com>
2026-08-20 09:28:45 +0530

    cube

---
+++ cubeofcube.py
@@ -0,0 +1,8 @@
+# Define your base number
+number = 2
+
+# Calculate the cube of the cube (x ** 3 ** 3 or x ** 9)
+result = (number ** 3) ** 3
+
+# Display the output
+print(f"The cube of the cube of {number} is: {result}")
